f62a004a30dbf3c6e7dcb0df30e2bf653e28d48d
Sai <bhandarelaxmi144@gmail.com>
2026-01-22 22:48:27 +0530

    Sitemap xml

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const baseUrl = "https://kk-fire-services.vercel.app";
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
